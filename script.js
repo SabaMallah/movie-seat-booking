bacc53246a9eb44182c869a5225169d2f3c1f600
DESKTOP-IRIQRDG\ADMIN <mallahsaba28@gmail.com>
2022-10-26 13:34:06 +0500

    Setting data to local storage

--- script.js
+++ script.js
@@ -6,9 +6,20 @@ const movieSelect = document.getElementById("movie");
 
 let ticketPrice = +movieSelect.value;
 
+//Save  selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem("selectedMovieIndex", movieIndex);
+  localStorage.setItem("selectedMoviePrice", moviePrice);
+}
+
 //Update Total and count
 function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll(".row .seat.selected");
+
+  const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+
+  localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
+
   const selectedSeatsCount = selectedSeats.length;
   count.innerText = selectedSeatsCount;
   total.innerText = selectedSeatsCount * ticketPrice;
@@ -17,6 +28,8 @@ function updateSelectedCount() {
 // movie select eevent
 movieSelect.addEventListener("change", (e) => {
   ticketPrice = +e.target.value;
+  setMovieData(e.target.selectedIndex, e.target.value);
+
   updateSelectedCount();
 });
 
