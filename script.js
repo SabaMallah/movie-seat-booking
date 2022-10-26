c9ff0d286e81bc9ac25ed6ba6cc73a3257a7b693
DESKTOP-IRIQRDG\ADMIN <mallahsaba28@gmail.com>
2022-10-26 13:54:48 +0500

    Populate UI with saved data

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const count = document.getElementById("count");
 const total = document.getElementById("total");
 const movieSelect = document.getElementById("movie");
 
+populateUi();
+
 let ticketPrice = +movieSelect.value;
 
 //Save  selected movie index and price
@@ -17,12 +19,30 @@ function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll(".row .seat.selected");
 
   const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
-
   localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
 
   const selectedSeatsCount = selectedSeats.length;
   count.innerText = selectedSeatsCount;
   total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+// Get data from local storage and populate UI
+function populateUi() {
+  const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
+
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add("selected");
+      }
+    });
+  }
+
+  const selectedMovieIndex = localStorage.getItem("selectedMovieIndex");
+
+  if (selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex;
+  }
 }
 
 // movie select eevent
@@ -44,3 +64,6 @@ container.addEventListener("click", (e) => {
 
   updateSelectedCount();
 });
+
+//initial count and total set
+updateSelectedCount();
